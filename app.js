e387a687eb04ff0c49b5d0dad110d8f09be42533
yender <hernandezyender04@gmail.com>
2023-04-22 01:39:42 -0400

    yenderpro0

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app  = express()
+app.listen(3000)
+
+app.get("/",function(resq, res){
+    res.send('Yender Osorio 30626481 seccion 3');
+    
+} ) 
+
+
+
+
